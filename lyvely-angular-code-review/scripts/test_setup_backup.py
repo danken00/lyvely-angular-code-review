2e43a3d2151eebef963c7846cc8fa7de9b870ad2
Dan Kennedy <dan@mojoplease.com>
2023-07-22 16:48:13 +0400

    Update test_setup_backup.py

--- lyvely-angular-code-review/scripts/test_setup_backup.py
+++ lyvely-angular-code-review/scripts/test_setup_backup.py
@@ -1,10 +1,16 @@
 # This file helps set up a PR for review. Effectively, it helps empty the list of files to be reviewed and then
 #  recreating them so we can create a PR with those files.
 #
-# 1. To use, create a new branch we will make the changes to, in order to merge back to `main`
-# 2. Switch to that branch
-# 3. Back up the files so we can empty them and recreate them: `python3 test_setup_backup.py backup`
-# 4. Commit the
+# 1. In the `main` branch, back up the files so we can empty them and recreate them later:
+#     `python3 test_setup_backup.py backup`
+# 2. Commit the emptied files. The backup files should still be there and Git should ignore them
+# 3. Create a new branch from `main`, and switch to it e.g. `release/v1.0.0`. Make sure you "bring" the `.BAK` files
+#     with you to the new branch
+# 4. Now restore the backup files: `python3 test_setup_backup.py setup`
+# 5. Commit those files to the release branch. Now you should have `main` with those files emptied, and your new branch
+#     with the original files
+# 6. Now, when someone forks the repo, they should be able to create a PR from the release branch to main and leave
+#     comments and what not
 
 import shutil
 import sys
